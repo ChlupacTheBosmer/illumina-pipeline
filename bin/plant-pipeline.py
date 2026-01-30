e7a7114e04c2ca01135bcc5fb802181b8060f408
Petr <petaschlup@seznam.cz>
2026-01-30 21:28:24 +0100

    Print length of grouped FASTQ files
    Add print statement to show the length of grouped files.

--- bin/plant-pipeline.py
+++ bin/plant-pipeline.py
@@ -512,6 +512,7 @@ def do_run_trim_and_pair_original_fastq_files(config):
             find_files(original_files_dir(config), '*.fastq.gz'),
             r"^(.*)_R\d+_")
 
+    print("len: ", len(grouped_files))
     for g in grouped_files:
         for i, f in enumerate(g):
             print(f)
